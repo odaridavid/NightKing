537b9f2e555bafb625a30d7272af48a9a5039ca5
Davidodari <davidkibzodari@gmail.com>
2019-06-14 13:18:45 +0300

    Set Default Mode

--- app/src/main/java/com/gdg/demo/nightking/NightKingApplication.kt
+++ app/src/main/java/com/gdg/demo/nightking/NightKingApplication.kt
@@ -1,9 +1,16 @@
 package com.gdg.demo.nightking
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
-class NightKingApplication: Application(){
+class NightKingApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+//        TODO 1. Tell AppCompat What Mode To Use from available modes
+//        MODE_NIGHT_YES
+//        MODE_NIGHT_NO
+//        MODE_NIGHT_AUTO_BATTERY
+//        MODE_NIGHT_FOLLOW_SYSTEM
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
     }
 }
