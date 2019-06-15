22bad0318a40d47be3a32f333936d7d6764a37a8
Davidodari <davidkibzodari@gmail.com>
2019-06-15 03:30:47 +0300

    Finish up on Settings

--- app/src/main/java/com/gdg/demo/nightking/ui/main/MainActivity.kt
+++ app/src/main/java/com/gdg/demo/nightking/ui/main/MainActivity.kt
@@ -2,8 +2,11 @@ package com.gdg.demo.nightking.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.gdg.demo.nightking.NightKingApplication
 import com.gdg.demo.nightking.R
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,24 @@ class MainActivity : AppCompatActivity() {
 //        val isNight =
 //            resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
 //        Timber.d("is Night Theme = $isNight")
+        (application as NightKingApplication).repo
+            .nightModeLive.observe(this, Observer { nightMode ->
+            nightMode?.let { theme ->
+                when (theme) {
+                    "Light" -> {
+                        delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+                    }
+                    "System" -> {
+                        delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    }
+                    "Dark" -> {
+                        delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+                    }
+                }
+
+            }
+        }
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/gdg/demo/nightking/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/gdg/demo/nightking/ui/settings/SettingsFragment.kt
@@ -2,6 +2,8 @@ package com.gdg.demo.nightking.ui.settings
 
 
 import android.os.Bundle
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.gdg.demo.nightking.R
 
@@ -9,7 +11,21 @@ import com.gdg.demo.nightking.R
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        val themePreference: ListPreference? = findPreference(THEME_KEY)
+        val themeArrayOptions = resources.getStringArray(R.array.theme_options)
+        val themeArrayValues = resources.getStringArray(R.array.theme_values)
+        themePreference?.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
+            when (preference.value) {
+                themeArrayValues[0] -> themeArrayOptions[0]
+                themeArrayValues[1] -> themeArrayOptions[1]
+                themeArrayValues[2] -> themeArrayOptions[2]
+                else -> getString(R.string.def)
+            }
+        }
     }
 
+    companion object {
+        const val THEME_KEY = "theme"
+    }
 
 }
