6b647bccd020a990afbe35be131c71192635e8ec
Davidodari <davidkibzodari@gmail.com>
2019-07-03 15:45:36 +0300

    create navigation graph for transport activity

---
+++ app/src/main/java/com/gdg/demo/nightking/ui/TransportActivity.kt
@@ -0,0 +1,13 @@
+package com.gdg.demo.nightking.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gdg.demo.nightking.R
+
+class TransportActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transport)
+    }
+}
--- app/src/main/java/com/gdg/demo/nightking/ui/transport/TransportFragment.kt
+++ app/src/main/java/com/gdg/demo/nightking/ui/transport/TransportFragment.kt
@@ -2,11 +2,11 @@ package com.gdg.demo.nightking.ui.transport
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.gdg.demo.nightking.R
 
 
@@ -15,6 +15,9 @@ import com.gdg.demo.nightking.R
  */
 class TransportFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +25,6 @@ class TransportFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_transport, container, false)
     }
+
 
 }
