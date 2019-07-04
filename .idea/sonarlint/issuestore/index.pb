ba1813273ee78d3dab323f85f401bd5b1aee7b20
Harun Wangereka <harunwangereka@gmail.com>
2019-07-04 16:23:48 +0300

    added collapsing toolbar

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+p
+@app/src/main/java/com/gdg/demo/nightking/NightKingApplication.kt,0/5/0567955898fa6d43e7a1444d23fbe013d713cc07
+k
+;app/src/main/java/com/gdg/demo/nightking/ui/MainActivity.kt,b/d/bd7b0a8cc7e0fcbae1dc7e5cd317383706ea44f7
+j
+:app/src/main/java/com/gdg/demo/nightking/ui/TestDetails.kt,5/4/542051aa7257d889573a502dd1aca894ee50942e
+y
+Iapp/src/main/java/com/gdg/demo/nightking/ui/widgets/CollapsibleToolbar.kt,1/c/1c8911744111b94ab8370232ac5cec04fc784c65
+p
+@app/src/main/java/com/gdg/demo/nightking/ui/main/DemoFragment.kt,8/1/81834b6df7a1b65921f6c7c1864e3b98f9b87ce0
--- app/src/main/java/com/gdg/demo/nightking/ui/main/DemoFragment.kt
+++ app/src/main/java/com/gdg/demo/nightking/ui/main/DemoFragment.kt
@@ -6,11 +6,13 @@ import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.gdg.demo.nightking.R
 import com.gdg.demo.nightking.data.dummyData
+import kotlinx.android.synthetic.main.fragment_demo.*
 
 /**
  * A simple [Fragment] subclass.
@@ -31,6 +33,13 @@ class DemoFragment : Fragment() {
         recyclerView.setHasFixedSize(true)
         setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        materialCard.setOnClickListener {
+            findNavController().navigate(R.id.actiondemoFragment_to_testFragment)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
