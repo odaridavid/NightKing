03bf14d0d65ed06fe00267e7d74fef8cbda9f148
David Odari <davidkibzodari@gmail.com>
2020-03-29 13:36:53 +0300

    improve readability

--- app/src/main/java/com/gdg/demo/nightking/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/gdg/demo/nightking/ui/settings/SettingsFragment.kt
@@ -13,19 +13,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         val themePreference: ListPreference? = findPreference(THEME_KEY)
+        
         val themeArrayOptionsAboveQ = resources.getStringArray(R.array.theme_options_above_q)
         val themeArrayOptionsBelowQ = resources.getStringArray(R.array.theme_options_below_q)
         themePreference?.entries = if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ else themeArrayOptionsBelowQ
-        val themeArrayValues = resources.getStringArray(R.array.theme_values)
+        
         themePreference?.summaryProvider = Preference.SummaryProvider<ListPreference> { preference ->
             when (preference.value) {
-//                 Light Theme
-                themeArrayValues[0] -> themeArrayOptionsAboveQ[0]
-//                 Dark Theme
-                themeArrayValues[1] -> themeArrayOptionsAboveQ[1]
-//                 System Defined Theme/Auto Battery
-                themeArrayValues[2] -> if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ[2] else themeArrayOptionsBelowQ[2]
-                else -> getString(R.string.def)
+                 "Light"  -> themeArrayOptionsAboveQ[0]
+                 "Dark"   -> themeArrayOptionsAboveQ[1]
+                 "System" -> if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ[2] else themeArrayOptionsBelowQ[2]
+                  else -> getString(R.string.def)
             }
         }
     }
